0ba7d4f3bda96da72b66e77a98ccb0144828df03
i-safronov <ilya.safa1337w@gmail.com>
2024-10-30 11:00:18 +0500

    Changed padding of header in the CanvasScreen.kt

--- app/src/main/java/com/safronov/livepictures/ui/composable/CanvasScreen.kt
+++ app/src/main/java/com/safronov/livepictures/ui/composable/CanvasScreen.kt
@@ -431,7 +431,7 @@ private fun TopBar(
 ) {
     Row(
         modifier = Modifier
-            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .padding(top = 12.dp, start = 8.dp, end = 8.dp)
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
