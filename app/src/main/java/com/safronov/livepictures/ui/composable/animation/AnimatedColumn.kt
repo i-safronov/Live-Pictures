d54445342ad5a219101df7898316a829ef34178e
i-safronov <ilya.safa1337w@gmail.com>
2024-11-01 19:26:06 +0500

    Improved AnimatedColumn.kt

--- app/src/main/java/com/safronov/livepictures/ui/composable/animation/AnimatedColumn.kt
+++ app/src/main/java/com/safronov/livepictures/ui/composable/animation/AnimatedColumn.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Path
@@ -23,12 +24,15 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.safronov.livepictures.ui.composable.PathData
+import com.safronov.livepictures.ui.theme.Colors
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun AnimatedColumn(
     modifier: Modifier = Modifier,
-    duration: Int = 2000,
+    duration: Int = 1000,
     animation: List<PathData>,
 ) {
     Column(
@@ -37,6 +41,7 @@ fun AnimatedColumn(
         var index by remember { mutableStateOf(0) }
         var currentPathData by remember { mutableStateOf(animation.getOrNull(index)) }
         val strokeWidthPx = with(LocalDensity.current) { 8.dp.toPx() }
+        val scope = rememberCoroutineScope()
 
         val completedPaths = remember { mutableStateListOf<PathData>() }
 
@@ -58,15 +63,27 @@ fun AnimatedColumn(
                     )
 
                     completedPaths.add(currentPathData!!)
+
                     if (index < animation.lastIndex) {
                         index += 1
                         currentPathData = animation.getOrNull(index)
                     } else {
-                        currentPathData = null
+                        completedPaths.clear()
+                        index = 0
+                        currentPathData = PathData(
+                            path = Path(),
+                            color = Colors.Transparent,
+                            frameId = -1,
+                        )
+                        scope.launch {
+                            delay(500)
+                            currentPathData = animation[index]
+                        }
                     }
                 }
             }
 
+            // Create a derived state for the animated path
             val animatedPath = remember(currentPathData) {
                 derivedStateOf {
                     val destination = Path()
@@ -78,6 +95,7 @@ fun AnimatedColumn(
             }
 
             Canvas(modifier = Modifier.fillMaxWidth()) {
+                // Draw the animated path if there is progress
                 if (currentPathData != null && animatedProgress.value > 0f) {
                     drawPath(
                         animatedPath.value,
@@ -85,6 +103,7 @@ fun AnimatedColumn(
                         style = Stroke(width = strokeWidthPx)
                     )
                 }
+                // Draw all completed paths
                 completedPaths.forEach { pathData ->
                     drawPath(pathData.path, pathData.color, style = Stroke(width = strokeWidthPx))
                 }
@@ -92,7 +111,6 @@ fun AnimatedColumn(
         }
     }
 }
-
 
 
 
