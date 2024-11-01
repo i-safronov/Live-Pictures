b89cda7abb6d854b52ddb2b56e00e456d89a64ff
i-safronov <ilya.safa1337w@gmail.com>
2024-11-01 19:01:24 +0500

    Improved animation of lines in the AnimatedColumn.kt

--- app/src/main/java/com/safronov/livepictures/ui/composable/CanvasScreen.kt
+++ app/src/main/java/com/safronov/livepictures/ui/composable/CanvasScreen.kt
@@ -1,6 +1,5 @@
 package com.safronov.livepictures.ui.composable
 
-import android.util.Log
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
@@ -29,28 +28,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.safronov.livepictures.R
 import com.safronov.livepictures.udf.onEvent
-import com.safronov.livepictures.ui.composable.CanvasContract.*
+import com.safronov.livepictures.ui.composable.CanvasContract.Event
+import com.safronov.livepictures.ui.composable.CanvasContract.Executor
+import com.safronov.livepictures.ui.composable.CanvasContract.State
 import com.safronov.livepictures.ui.composable.CanvasContract.State.UserInputType
 import com.safronov.livepictures.ui.composable.animation.AnimatedColumn
-import com.safronov.livepictures.ui.composable.animation.AnimationDialog
 import com.safronov.livepictures.ui.theme.ColorValue
 import com.safronov.livepictures.ui.theme.Colors
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 data class PathData(
     val path: Path = Path(),
--- app/src/main/java/com/safronov/livepictures/ui/composable/animation/AnimatedColumn.kt
+++ app/src/main/java/com/safronov/livepictures/ui/composable/animation/AnimatedColumn.kt
@@ -17,17 +17,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Matrix
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.safronov.livepictures.ui.composable.PathData
-import com.safronov.livepictures.ui.theme.Colors
-import kotlin.math.min
 
 @SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
@@ -40,111 +35,64 @@ fun AnimatedColumn(
         modifier = modifier.fillMaxSize()
     ) {
         var index by remember { mutableStateOf(0) }
-        var currentPath by remember { mutableStateOf(animation.getOrNull(index)) }
+        var currentPathData by remember { mutableStateOf(animation.getOrNull(index)) }
         val strokeWidthPx = with(LocalDensity.current) { 8.dp.toPx() }
 
-        val completedPaths = remember { mutableStateListOf<Pair<Path, Color>>() }
+        val completedPaths = remember { mutableStateListOf<PathData>() }
 
-        if (currentPath != null) {
-            BoxWithConstraints(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                val pathMeasure = remember { PathMeasure() }
-                pathMeasure.setPath(currentPath!!.path, false)
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val pathMeasure = remember { PathMeasure() }
+            val animatedProgress = remember { Animatable(0f) }
 
-                val animatedProgress = remember { Animatable(0f) }
-
-                LaunchedEffect(currentPath) {
+            if (currentPathData != null) {
+                LaunchedEffect(currentPathData) {
                     animatedProgress.snapTo(0f)
+
+                    pathMeasure.setPath(currentPathData!!.path, false)
+
                     animatedProgress.animateTo(
                         targetValue = pathMeasure.length,
                         animationSpec = tween(duration)
                     )
 
-                    completedPaths.add(currentPath!!.path to currentPath!!.color)
-
+                    completedPaths.add(currentPathData!!)
                     if (index < animation.lastIndex) {
                         index += 1
-                        currentPath = animation.getOrNull(index)
+                        currentPathData = animation.getOrNull(index)
+                    } else {
+                        currentPathData = null
                     }
-                }
-
-                val animatedPath = remember {
-                    derivedStateOf {
-                        val destination = Path()
-                        pathMeasure.getSegment(0f, animatedProgress.value, destination)
-                        destination
-                    }
-                }
-
-                Canvas(modifier = Modifier.fillMaxWidth()) {
-                    completedPaths.forEach { (path, color) ->
-                        drawPath(path, color, style = Stroke(width = strokeWidthPx))
-                    }
-
-                    drawPath(animatedPath.value, currentPath!!.color, style = Stroke(width = strokeWidthPx))
                 }
             }
-        }
-    }
-}
 
-
-
-@SuppressLint("UnusedBoxWithConstraintsScope")
-@Composable
-fun AnimatedPath(
-    modifier: Modifier = Modifier,
-    pathStr: Path,
-    strokeWidth: Dp,
-    color: Color = Colors.Blue,
-    duration: Int,
-) {
-    with(LocalDensity.current) {
-        BoxWithConstraints(modifier) {
-            var path by remember { mutableStateOf(Path()) }
-            val strokeWidthPx = strokeWidth.toPx()
-
-            val pathMeasure = remember { PathMeasure() }
-            pathMeasure.setPath(path, false)
-
-            val animatedProgress = remember { Animatable(0f) }
-
-            LaunchedEffect(pathMeasure.length) {
-                animatedProgress.animateTo(
-                    targetValue = pathMeasure.length,
-                    animationSpec = tween(duration)
-                )
-            }
-
-            val animatedPath = remember {
+            val animatedPath = remember(currentPathData) {
                 derivedStateOf {
                     val destination = Path()
-                    pathMeasure.getSegment(0f, animatedProgress.value, destination)
+                    if (currentPathData != null) {
+                        pathMeasure.getSegment(0f, animatedProgress.value, destination)
+                    }
                     destination
                 }
             }
 
             Canvas(modifier = Modifier.fillMaxWidth()) {
-                drawPath(animatedPath.value, color, style = Stroke(width = strokeWidthPx))
+                if (currentPathData != null && animatedProgress.value > 0f) {
+                    drawPath(
+                        animatedPath.value,
+                        currentPathData!!.color,
+                        style = Stroke(width = strokeWidthPx)
+                    )
+                }
+                completedPaths.forEach { pathData ->
+                    drawPath(pathData.path, pathData.color, style = Stroke(width = strokeWidthPx))
+                }
             }
         }
     }
 }
 
 
-fun Path.fillBounds(strokeWidthPx: Float, maxWidth: Int, maxHeight: Int) {
-    val pathSize = getBounds()
-    val matrix = Matrix()
 
-    val horizontalOffset = pathSize.left - strokeWidthPx / 2
-    val verticalOffset = pathSize.top - strokeWidthPx / 2
-    val scaleWidth = maxWidth / (pathSize.width + strokeWidthPx)
-    val scaleHeight = maxHeight / (pathSize.height + strokeWidthPx)
-    val scale = min(scaleHeight, scaleWidth)
 
-    matrix.scale(scale, scale)
-    matrix.translate(-horizontalOffset, -verticalOffset)
-
-    transform(matrix)
-}
